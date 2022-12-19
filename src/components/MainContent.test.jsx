cb45831da96596abb2fc0526f19cba382830cf2b
webhasan <webhasan24@gmail.com>
2022-12-19 12:36:32 +0600

    Fail test

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -8,7 +8,7 @@ describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
 
-    expect(screen.getByRole('button')).toBeInTheDocument();
+    expect(screen.getByRole('button')).not().toBeInTheDocument();
   });
 
   it('should show the help area after clicking the button', async () => {
